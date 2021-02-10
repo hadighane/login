fbfcca35c10a3b849fa274653c9415a2a52f8faa
hadighane <ce.negro@yahoo.com>
2021-02-10 16:49:11 +0330

    update model.php
    adding insert, update and delete query function to the model class.
    22 Bahman 99

--- model.php
+++ model.php
@@ -49,5 +49,93 @@ class model{
 		$select_query -> execute();
 		return $select_query->fetchall();	
 	}
-
+	function insert ($TableName, $ColumnNames=[], $Values=[])
+	{
+		$query = "INSERT INTO ";
+		if(count($ColumnNames)>1)
+		{
+			foreach ($ColumnNames as $ColumnName){
+				$Columns .= ($ColumnName. ", ");
+			}
+		}
+		else{
+			foreach ($ColumnNames as $ColumnName){
+				$Columns = $ColumnName;
+			}
+		}
+		
+		$query .=  $TableName ." (" .$Columns .") VALUES (";
+		
+		if(count($Values)>1){
+			foreach ($Values as $Value){
+				$Value_string .= ($Value. ", ");
+			}
+		}
+		else{
+			foreach ($Values as $Value){
+				$Value_string = $Value;
+			}
+		}
+		$query .= $Value_string. ")";
+		$insert_query = $this->conn->prepare($query);
+		$insert_query -> execute();
+		return $insert_query->fetchall();
+	}
+	function update ($TableName,$ColumnNames=[],$Values=[],$Wheres=[])
+	{
+		$query = "UPDATE ". $TableName. " SET ";
+		if(count($ColumnNames) > 1)
+		{
+			$Value = 0;
+			foreach ($ColumnNames as $ColumnName){
+				$set_string .= ($ColumnName. " = ". $Values[$Value]. ", ");
+				$Value = $Value + 1;
+			}
+		}
+		else {
+			$set_string = $ColumnNames[0]. " = ". $Values[0]. ", ";
+		}
+		$query .= $set_string. " Where ";
+		
+		if(count($Wheres))
+		{
+			foreach ($Wheres as $Where_key=>$Where_Value)
+			{
+				if(count($Wheres)==1)
+				{
+					$Where_string = $Where_key. '"'. $Where_Value. '"';
+				}
+				else
+				{
+					$Where_string .= $Where_key. '"'. $Where_Value. '"'. " AND ";
+				}
+			}
+		$query .= $Where_string;
+		}
+		$update_query = $this->conn->prepare($query);
+		$update_query -> execute();
+		return $update_query->fetchall();
+	}
+	function delete ($TableName, $Wheres)
+	{
+		$query = "DELETE FROM ". $TableName. " WHERE ";
+		if(count($Wheres))
+		{
+			foreach ($Wheres as $Where_key=>$Where_Value)
+			{
+				if(count($Wheres)==1)
+				{
+					$Where_string = $Where_key. '"'. $Where_Value. '"';
+				}
+				else
+				{
+					$Where_string .= $Where_key. '"'. $Where_Value. '"'. " AND ";
+				}
+			}
+		$query .= $Where_string;
+		}
+		$delete_query = $this->conn->prepare($query);
+		$delete_query -> execute();
+		return $delete_query->fetchall();
+	}
 }
